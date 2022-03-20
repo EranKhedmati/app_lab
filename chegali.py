2249e97e456faaa70dcda9c4ca13968ae7184d02
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2022-03-20 09:47:12 +0330

    ساخت باکس های درباره چگالی و محسبه

--- chegali.py
+++ chegali.py
@@ -1,7 +1,7 @@
 #-----------------------------------
 #imports
 #-----------------------------------
-from guizero import App , Box
+from guizero import App , Box , Text
 
 #-----------------------------------
 #variables
@@ -11,5 +11,15 @@ from guizero import App , Box
 #App
 #-----------------------------------
 app = App("chegali" , width=400 , height=540)
+#-----------------------------------
+#boxs
+#-----------------------------------
+about_box = Box(app , align="up" , border=True , width="fill" , height=200) #بردر برای اینکه باکس رو فعلا تشخیض بدیم
+
+# chegali_text = Text(about_box , text="چگالی یا دانستیه یک ماده که به آن جرم حجمی یا جرم ویژه می گویند ، یا به عبارتی دیگر جرم یک سانتی متر مکعب از یک جسم را می گویند و آن را با نماد p نشان می دهند و از رابطه ی p=m/v به دست می آید  که p چگالی ، mحجم و v حجم جسم است")
+
+calculation_box = Box(app , width="fill" , height=200 , border=True
+
+    )
 
 app.display()
