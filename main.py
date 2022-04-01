8d8a00304afc8031796bc39e601818fe3819e99f
Erfan Khedmati <erfankhedmati15@gmail.com>
2022-04-01 22:51:00 +0430

    اصلاح فانکشن باز کردن کلید ها

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -----------------------------------
 # Imports
 # -----------------------------------
-from guizero import App, PushButton, Box, Picture, Text, Window , MenuBar
+from guizero import App, PushButton, Box, Picture, Text, Window, MenuBar
 import os
 # -----------------------------------
 # Variables
@@ -14,6 +14,7 @@ import os
 # -----------------------------------
 # creat board
 # -----------------------------------
+
 
 def board():
     buttons = Box(app, layout="grid")
@@ -44,51 +45,60 @@ def board():
 
     # -------------------------------
     # Open buttons
-    #--------------------------------
+    # --------------------------------
     def open_window(name: str):
         if name == "chegali":
             chegali_window.show()
+            app.hide()
         elif name == "blood":
             blood_window.show()
+            app.hide()
         elif name == "meghias":
             meghias_window.show()
+            app.hide()
         elif name == "takhalkhol":
             takhalkhol_window.show()
+            app.hide()
         elif name == "test":
             test_window.show()
+            app.hide()
         else:
             Warning_window.show()
+            app.hide()
     # -------------------------------
     # Picturs of buttons
     # -------------------------------
     chegali_botton = PushButton(
-        buttons, command=open_window, image="icons\\chegali.png", grid=[0, 0], args=["chegali"])
+        buttons, command=open_window, image="icons/chegali.png", grid=[0, 0], args=["chegali"])
 
     blood_button = PushButton(
-        buttons, command=open_window, image="icons\\blood.png", grid=[1, 0], args=["blood"])
+        buttons, command=open_window, image="icons/blood.png", grid=[1, 0], args=["blood"])
 
     meghias_button = PushButton(
-        buttons, command=open_window, image="icons\\meghias.png", grid=[0, 1], args=["meghias"])
+        buttons, command=open_window, image="icons/meghias.png", grid=[0, 1], args=["meghias"])
 
     takhalkhol_button = PushButton(
-        buttons, command=open_window, image="icons\\takhalkhol.png", grid=[1, 1], args=["takhalkhol"])
+        buttons, command=open_window, image="icons/takhalkhol.png", grid=[1, 1], args=["takhalkhol"])
 
     test_button = PushButton(buttons, command=open_window,
-                             image="icons\\test.png", grid=[0, 2], args=["test"])
+                             image="icons/test.png", grid=[0, 2], args=["test"])
 
     warning_button = PushButton(
-        buttons, command=open_window, image='icons\\warning.png', grid=[1, 2], args=["warning"])
+        buttons, command=open_window, image='icons/warning.png', grid=[1, 2], args=["warning"])
+
 
 def about():
-    about_window = Window(app , title="درباره" , width=400 , height=540)
+    about_window = Window(app, title="درباره", width=400, height=540)
+
+
 # ------------------------------------
 # App
 # ------------------------------------
 app = App('app_lab')
 
-title = Text(app , text="آزمایشگاه علوم تجربی دهم" , size=25 , font="B Titr")
+title = Text(app, text="آزمایشگاه علوم تجربی دهم", size=25, font="B Titr")
 
-about_menubar = MenuBar(app,toplevel=["منو"], options=[[["درباره" , about]]] )
+about_menubar = MenuBar(app, toplevel=["منو"], options=[[["درباره", about]]])
 
 # menu_title()
 board()
