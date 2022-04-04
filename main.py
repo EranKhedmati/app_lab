7ef3e08434048da58bdf2f1e3e5b527f74f057c2
Erfan Khedmati <erfankhedmati15@gmail.com>
2022-04-05 03:43:53 +0430

    ادامه ساخت پنجره چگالی و تغییر ساختار کد

--- main.py
+++ main.py
@@ -1,88 +1,125 @@
 # -----------------------------------
 # Imports
 # -----------------------------------
-from guizero import App, PushButton, Box, Picture, Text, Window, MenuBar
-import os
+from pydoc import text
+from turtle import width
+from guizero import App, PushButton, Box, Picture,TextBox, Text, Window, MenuBar,ButtonGroup
+
+from library import chegali
+
+# -----------------------------------
+# CONSTANS
+# -----------------------------------
+MAIN_BOX = int(0)
+GOROOH_KHOONEI_BOX = int(1)
+CHEGHALEY_BOX = int(2)
+TAKHALKHOL_BOX = int(3)
+MEGHYAS_BOX = int(4)
+ALAMAT_BOX = int(5)
+AZMOON_BOX = int(6)
+
 # -----------------------------------
 # Variables
 # -----------------------------------
+def showBox(id:int):
+    boxMain.hide()
+    if id==CHEGHALEY_BOX:
+        boxChegalei.show()
 
 # -----------------------------------
 # Functions
 # -----------------------------------
+# Chegali Section
+def updateBtnGChegali():
+    txtChegli.enable()
+    txtJerm.enable()
+    txtHajm.enable()
+    if btnGChegali.value=="چگالی":
+        txtChegli.disable()
+    elif btnGChegali.value=="جرم":
+        txtJerm.disable()
+    elif btnGChegali.value=="حجم":
+        txtHajm.disable()
 
-# -----------------------------------
-# creat board
-# -----------------------------------
+def chegaliParametersChange():
+    try:
+        p = float(txtChegli.value)
+    except ValueError:
+        txtChegli.value=""
 
+    try:
+        v = float(txtHajm.value)
+    except ValueError:
+        txtHajm.value=""
 
-def board():
-    buttons = Box(app, layout="grid")
-    for x in range(2):
-        for y in range(3):
-            button = PushButton(buttons, grid=[x, y], padx=60, pady=60)
+    try:
+        m = float(txtJerm.value)
+    except ValueError:
+        txtJerm.value=""
 
-    # -------------------------------
-    # Open buttons
-    # --------------------------------
-    def open_window(name: str):
-        if name == "chegali":
-            chegali_window()
-        elif name == "blood":
-            blood_window.show()
-            app.hide()
-        elif name == "meghias":
-            meghias_window.show()
-            app.hide()
-        elif name == "takhalkhol":
-            takhalkhol_window.show()
-            app.hide()
-        elif name == "test":
-            test_window.show()
-            app.hide()
-        else:
-            Warning_window.show()
-            app.hide()
-    # -------------------------------
-    # Picturs of buttons
-    # -------------------------------
-    chegali_botton = PushButton(
-        buttons, command=open_window, image="icons/chegali.png", grid=[0, 0], args=["chegali"])
+    if txtChegli.enabled==False and m>-1 and v>-1:
+        p=int(-1)
+        txtChegli.value=chegali(p,m,v)
+    elif txtHajm.enabled==False and m>-1 and p>-1:
+        v=int(-1)
+        txtHajm.value=chegali(p,m,v)
+    elif txtJerm.enabled==False and p>-1 and v>-1:
+        m=int(-1)
+        txtJerm.value=chegali(p,m,v)
+# ---------------------------------------------
 
-    blood_button = PushButton(
-        buttons, command=open_window, image="icons/blood.png", grid=[1, 0], args=["blood"])
-
-    meghias_button = PushButton(
-        buttons, command=open_window, image="icons/meghias.png", grid=[0, 1], args=["meghias"])
-
-    takhalkhol_button = PushButton(
-        buttons, command=open_window, image="icons/takhalkhol.png", grid=[1, 1], args=["takhalkhol"])
-
-    test_button = PushButton(buttons, command=open_window,
-                             image="icons/test.png", grid=[0, 2], args=["test"])
-
-    warning_button = PushButton(
-        buttons, command=open_window, image='icons/warning.png', grid=[1, 2], args=["warning"])
-
-    def chegali_window():
-        buttons.hide()
-        title.hide()
-        about_chegali = Box(app , border=True , width="fill" , height=150)
-        chegali_text = Text(about_chegali , text="چگالی یا دانستیه یک ماده که به آن جرم\n حجمی یا جرم ویژه می گویند \n، یا به عبارتی دیگر جرم یک سانتی متر \nمکعب از یک جسم را می گویند  \nو آن را با نماد p نشان می دهند و از رابطه ی p=m/vبه \nدست می آید  که p چگالی ، mحجم و v حجم جسم است")
-        chegali_cal_box = Box(app , width="fill" , height=250 , border=True) #cal = calculate
-        footer_box = Box(app , width="fill" ,height=100 , border=True)
 
 def about():
     about_window = Window(app, title="درباره", width=400, height=540)
 
+
 # ------------------------------------
 # App
 # ------------------------------------
-app = App('app_lab' , width=500 , height=500)
+app = App('app_lab', width=500, height=500)
 title = Text(app, text="آزمایشگاه علوم تجربی دهم", size=25, font="B Titr")
+
+# Main Box
+boxMain = Box(app, layout="grid")
+for x in range(2):
+    for y in range(3):
+        button = PushButton(boxMain, grid=[x, y], padx=60, pady=60)
+
+chegali_botton = PushButton(
+    boxMain, command=showBox, image="icons/chegali.png", grid=[0, 0], args=[CHEGHALEY_BOX])
+
+blood_button = PushButton(
+    boxMain, command=showBox, image="icons/blood.png", grid=[1, 0], args=[GOROOH_KHOONEI_BOX])
+
+meghias_button = PushButton(
+    boxMain, command=showBox, image="icons/meghias.png", grid=[0, 1], args=[MEGHYAS_BOX])
+
+takhalkhol_button = PushButton(
+    boxMain, command=showBox, image="icons/takhalkhol.png", grid=[1, 1], args=[TAKHALKHOL_BOX])
+
+test_button = PushButton(boxMain, command=showBox,
+                         image="icons/test.png", grid=[0, 2], args=[AZMOON_BOX])
+
+warning_button = PushButton(
+    boxMain, command=showBox, image='icons/warning.png', grid=[1, 2], args=[ALAMAT_BOX])
+
+
+# Box Chegalei
+boxChegalei = Box(app,visible=False,width='fill')
+btnGChegali = ButtonGroup(app, options=["چگالی", "حجم", "جرم"], selected="چگالی",
+    command=updateBtnGChegali)
+Text(boxChegalei,"لطفا دو مقدار را وارد کنید")
+Text(boxChegalei,"چگالی")
+txtChegli=TextBox(boxChegalei,width='fill',command=chegaliParametersChange,enabled=False)
+Text(boxChegalei,"حجم")
+txtHajm=TextBox(boxChegalei,width='fill',command=chegaliParametersChange)
+Text(boxChegalei,"جرم")
+txtJerm=TextBox(boxChegalei,width='fill',command=chegaliParametersChange)
+
+
+
 
 about_menubar = MenuBar(app, toplevel=["منو"], options=[[["درباره", about]]])
 
 # menu_title()
-board()
 app.display()
