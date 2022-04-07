1fc8a62ba4e865fa9e266181bf0e2573d42c566a
Erfan Khedmati <erfankhedmati15@gmail.com>
2022-04-07 22:37:37 +0430

    تغییر پنجره گروه خونی

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -----------------------------------
 # Imports
 # -----------------------------------
-from guizero import App, PushButton, Box, Picture,TextBox, Text, Window, MenuBar,ButtonGroup
+from guizero import App, PushButton, Box, Picture, TextBox, Text, Window, MenuBar, ButtonGroup, Slider
 
 from library import chegali
 from library import khoon
@@ -21,16 +21,22 @@ AZMOON_BOX = int(6)
 # -----------------------------------
 # Variables
 # -----------------------------------
-def showBox(id:int):
+
+
+def showBox(id: int):
     boxMain.hide()
-    if id==CHEGHALEY_BOX:
+    if id== MAIN_BOX:
+        boxMain.show()
+    elif id == CHEGHALEY_BOX:
         boxChegalei.show()
-    elif id==GOROOH_KHOONEI_BOX:
+    elif id == GOROOH_KHOONEI_BOX:
         boxKhoon.show()
-    elif id==MEGHYAS_BOX:
+    elif id == MEGHYAS_BOX:
         boxMeghias.show()
-    elif id==TAKHALKHOL_BOX:
+    elif id == TAKHALKHOL_BOX:
         boxTakhal.show()
+
+
 def hideBox():
     boxMain.show()
     boxChegalei.hide()
@@ -42,58 +48,75 @@ def hideBox():
 # Functions
 # -----------------------------------
 # Chegali Section
+
+
 def updateBtnGChegali():
     txtChegli.enable()
     txtJerm.enable()
     txtHajm.enable()
-    if btnGChegali.value=="چگالی":
+    if btnGChegali.value == "چگالی":
         txtChegli.disable()
-    elif btnGChegali.value=="جرم":
+    elif btnGChegali.value == "جرم":
         txtJerm.disable()
-    elif btnGChegali.value=="حجم":
+    elif btnGChegali.value == "حجم":
         txtHajm.disable()
+
 
 def chegaliParametersChange():
     try:
         p = float(txtChegli.value)
     except ValueError:
-        txtChegli.value=""
+        txtChegli.value = ""
 
     try:
         v = float(txtHajm.value)
     except ValueError:
-        txtHajm.value=""
+        txtHajm.value = ""
 
     try:
         m = float(txtJerm.value)
     except ValueError:
-        txtJerm.value=""
+        txtJerm.value = ""
 
-    if txtChegli.enabled==False and m>-1 and v>-1:
-        p=int(-1)
-        txtChegli.value=chegali(p,m,v)
-    elif txtHajm.enabled==False and m>-1 and p>-1:
-        v=int(-1)
-        txtHajm.value=chegali(p,m,v)
-    elif txtJerm.enabled==False and p>-1 and v>-1:
-        m=int(-1)
-        txtJerm.value=chegali(p,m,v)
-    resultKhoon.value = khoon(anti_a, anti_b, anti_d)
-#Groop Khooni section
+    if txtChegli.enabled == False and m > -1 and v > -1:
+        p = int(-1)
+        txtChegli.value = chegali(p, m, v)
+    elif txtHajm.enabled == False and m > -1 and p > -1:
+        v = int(-1)
+        txtHajm.value = chegali(p, m, v)
+    elif txtJerm.enabled == False and p > -1 and v > -1:
+        m = int(-1)
+        txtJerm.value = chegali(p, m, v)
+# Groop Khooni section
+# def khoonParametrChange():
+#     try:
+#         anti_a = int(txtAntia.value)
+#     except ValueError:
+#         txtAntia.value=""
+#     try:
+#         anti_b = int(txtAntib.value)
+#     except ValueError:
+#         txtAntib.value=""
+#     try:
+#         anti_d = int(txtAntid.value)
+#     except ValueError:
+#         txtAntid.value=""
+#     resultKhoon.value = khoon(anti_a, anti_b, anti_d)
+
+
 def khoonParametrChange():
     try:
-        anti_a = int(txtAntia.value)
+        anti_a = int(sldAntia.value)
     except ValueError:
-        txtAntia.value=""
+        anti_a = ""
     try:
-        anti_b = int(txtAntib.value)
+        anti_b = int(sldAntib.value)
     except ValueError:
-        txtAntib.value=""
+        anti_b = ""
     try:
-        anti_d = int(txtAntid.value)
+        anti_d = int(sldAntid.value)
     except ValueError:
-        txtAntid.value=""
-
+        anti_d = ""
     resultKhoon.value = khoon(anti_a, anti_b, anti_d)
 
 # Meghias section
@@ -101,12 +124,13 @@ def updateBtnMeghias():
     txtMeghias.enable()
     txtFmap.enable()
     txtFground.enable()
-    if btnMeghias.value=="مقیاس":
+    if btnMeghias.value == "مقیاس":
         txtMeghias.disable()
-    elif btnMeghias.value=="فاصله دو نقطه روی نقشه":
+    elif btnMeghias.value == "فاصله دو نقطه روی نقشه":
         txtFmap.disable()
-    elif btnMeghias.value=="فاصله دو نقطه روی زمین":
+    elif btnMeghias.value == "فاصله دو نقطه روی زمین":
         txtFground.disable()
+
 
 def meghiasParametersChange():
     try:
@@ -124,52 +148,54 @@ def meghiasParametersChange():
     except ValueError:
         txtFground.value = ""
 
-    if txtMeghias.enabled==False and f_map>-1 and f_ground>-1:
-        meghias=int(-1)
+    if txtMeghias.enabled == False and f_map > -1 and f_ground > -1:
+        meghias = int(-1)
         txtMeghias.value = takhmin_masafat(meghias, f_map, f_ground)
-    elif txtFmap.enabled==False and meghias>-1 and f_ground>-1:
-        f_map=int(-1)
-        txtFmap.value=takhmin_masafat(meghias, f_map, f_ground)
-    elif txtFground.enabled==False and f_map>-1 and f_ground>-1:
+    elif txtFmap.enabled == False and meghias > -1 and f_ground > -1:
+        f_map = int(-1)
+        txtFmap.value = takhmin_masafat(meghias, f_map, f_ground)
+    elif txtFground.enabled == False and f_map > -1 and f_ground > -1:
         f_ground = int(-1)
-        txtFground.value=takhmin_masafat(meghias, f_map, f_ground)
+        txtFground.value = takhmin_masafat(meghias, f_map, f_ground)
 # Takhalkhol section
+
 def updateBtnTakhal():
     txtTakhal.enable()
     txtVsang.enable()
     txtVkol.enable()
-    if btnTakhal.value=="تخلخل":
+    if btnTakhal.value == "تخلخل":
         txtTakhal.disable()
-    elif btnTakhal.value=="حجم فضای خالی سنگ یا رسوب":
+    elif btnTakhal.value == "حجم فضای خالی سنگ یا رسوب":
         txtVsang.disable()
-    elif btnTakhal.value=="حجم کل فضای سنگ یا رسوب":
+    elif btnTakhal.value == "حجم کل فضای سنگ یا رسوب":
         txtVkol.disable()
+
 
 def takhalParametersChange():
     try:
         takhalkhol = float(txtTakhal.value)
     except ValueError:
-        txtTakhal.value=""
+        txtTakhal.value = ""
 
     try:
         v_sang = float(txtVsang.value)
     except ValueError:
-        txtVsang.value=""
+        txtVsang.value = ""
 
     try:
         v_kol = float(txtVkol.value)
     except ValueError:
-        txtVkol.value=""
+        txtVkol.value = ""
 
-    if txtTakhal.enabled==False and v_sang>-1 and v_kol>-1:
-        takhalkhol=int(-1)
-        txtTakhal.value=cheshme(takhalkhol, v_sang, v_kol)
-    elif txtVsang.enabled==False and takhalkhol>-1 and v_kol>-1 :
-        v_sang=int(-1)
-        txtVsang.value=cheshme(takhalkhol, v_sang, v_kol)
-    elif txtVkol.enabled==False and v_sang>-1 and takhalkhol>-1:
-        v_kol=int(-1)
-        txtVkol.value=cheshme(takhalkhol, v_sang, v_kol)
+    if txtTakhal.enabled == False and v_sang > -1 and v_kol > -1:
+        takhalkhol = int(-1)
+        txtTakhal.value = cheshme(takhalkhol, v_sang, v_kol)
+    elif txtVsang.enabled == False and takhalkhol > -1 and v_kol > -1:
+        v_sang = int(-1)
+        txtVsang.value = cheshme(takhalkhol, v_sang, v_kol)
+    elif txtVkol.enabled == False and v_sang > -1 and takhalkhol > -1:
+        v_kol = int(-1)
+        txtVkol.value = cheshme(takhalkhol, v_sang, v_kol)
 
 # ---------------------------------------------
 
@@ -210,56 +236,72 @@ warning_button = PushButton(
 
 
 # Box Chegalei
-boxChegalei = Box(app,visible=False,width='fill')
+boxChegalei = Box(app, visible=False, width='fill')
 btnGChegali = ButtonGroup(boxChegalei, options=["چگالی", "حجم", "جرم"], selected="چگالی",
-    command=updateBtnGChegali)
-Text(boxChegalei,"لطفا دو مقدار را وارد کنید")
-Text(boxChegalei,"چگالی بر حسب رو")
-txtChegli=TextBox(boxChegalei,width='fill',command=chegaliParametersChange,enabled=False)
-Text(boxChegalei,"حجم بر حسب مترمکعب")
-txtHajm=TextBox(boxChegalei,width='fill',command=chegaliParametersChange)
-Text(boxChegalei,"جرم بر حسب کیلوگرم")
-txtJerm=TextBox(boxChegalei,width='fill',command=chegaliParametersChange)
-btnExitChegali=PushButton(boxChegalei,command=hideBox,text="صفحه ی اصلی")
+                          command=updateBtnGChegali)
+Text(boxChegalei, "لطفا دو مقدار را وارد کنید")
+Text(boxChegalei, "چگالی بر حسب رو")
+txtChegli = TextBox(boxChegalei, width='fill',
+                    command=chegaliParametersChange, enabled=False)
+Text(boxChegalei, "حجم بر حسب مترمکعب")
+txtHajm = TextBox(boxChegalei, width='fill', command=chegaliParametersChange)
+Text(boxChegalei, "جرم بر حسب کیلوگرم")
+txtJerm = TextBox(boxChegalei, width='fill', command=chegaliParametersChange)
+btnExitChegali = PushButton(boxChegalei, command=hideBox, text="صفحه ی اصلی")
 
 # Box Khoon
-boxKhoon = Box(app , visible=False , width='fill')
-Text(boxKhoon , "لطفا مشخص کنید اگر خون با ماده رسوب کرده است عدد ۱ و در غیر اینصورت عدد -۱ را وارد کنید")
-Text(boxKhoon , "آنتی A")
-txtAntia=TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
-Text(boxKhoon , 'آنتی B')
-txtAntib = TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
-Text(boxKhoon , 'آنتی D')
-txtAntid = TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
-resultKhoon = Text(boxKhoon)
-btnExitKhoon=PushButton(boxKhoon,command=hideBox,text="صفحه ی اصلی")
+
+# boxKhoon = Box(app , visible=False , width='fill')
+# Text(boxKhoon , "لطفا مشخص کنید اگر خون با ماده رسوب کرده است عدد ۱ و در غیر اینصورت عدد -۱ را وارد کنید")
+# Text(boxKhoon , "آنتی A")
+# txtAntia=TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
+# Text(boxKhoon , 'آنتی B')
+# txtAntib = TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
+# Text(boxKhoon , 'آنتی D')
+# txtAntid = TextBox(boxKhoon , width='fill' , command=khoonParametrChange)
+# resultKhoon = Text(boxKhoon)
+# btnExitKhoon=PushButton(boxKhoon,command=hideBox,text="صفحه ی اصلی")
+
+
+boxKhoon = Box(app, visible=False, width="fill")
+Text(boxKhoon, "لطفا در صورت رسوب خون عدد ۱ و در غیر این صورت عدد -۱ \n را وارد کنید")
+Text(boxKhoon, "آنتی A")
+sldAntia = Slider(boxKhoon, start=-1, end=1, command=khoonParametrChange)
+Text(boxKhoon, "آنتی B")
+sldAntib = Slider(boxKhoon, start=-1, end=1, command=khoonParametrChange)
+Text(boxKhoon, "آنتی D")
+sldAntid = Slider(boxKhoon, start=-1, end=1, command=khoonParametrChange)
+resultKhoon = Text(boxKhoon, size=20)
+btnExitKhoon = PushButton(boxKhoon, command=hideBox, text="صفحه ی اصلی")
 
 # Box meghias
-boxMeghias = Box(app , visible=False , width='fill')
-btnMeghias = ButtonGroup(boxMeghias , 
-options=["مقیاس" , "فاصله دو نقطه روی نقشه" , "فاصله دو نقطه روی زمین"] , command=updateBtnMeghias,selected="مقیاس")
-Text(boxMeghias , "مقیاس")
-txtMeghias = TextBox(boxMeghias , width='fill' , command=meghiasParametersChange,enabled=False)
-Text(boxMeghias , "فاصله روی نشه")
-txtFmap = TextBox(boxMeghias , width='fill' , command=meghiasParametersChange)
-Text(boxMeghias , "فاصله روی زمین")
-txtFground = TextBox(boxMeghias , width='fill' , command=meghiasParametersChange)
-btnExitMeghias=PushButton(boxMeghias,command=hideBox,text="صفحه ی اصلی")
+boxMeghias = Box(app, visible=False, width='fill')
+btnMeghias = ButtonGroup(boxMeghias,
+                         options=["مقیاس", "فاصله دو نقطه روی نقشه", "فاصله دو نقطه روی زمین"], command=updateBtnMeghias, selected="مقیاس")
+Text(boxMeghias, "مقیاس")
+txtMeghias = TextBox(boxMeghias, width='fill',
+                     command=meghiasParametersChange, enabled=False)
+Text(boxMeghias, "فاصله روی نشه")
+txtFmap = TextBox(boxMeghias, width='fill', command=meghiasParametersChange)
+Text(boxMeghias, "فاصله روی زمین")
+txtFground = TextBox(boxMeghias, width='fill', command=meghiasParametersChange)
+btnExitMeghias = PushButton(boxMeghias, command=hideBox, text="صفحه ی اصلی")
 
 # Box takhalkhol
-boxTakhal = Box(app , visible=False , width='fill')
-btnTakhal = ButtonGroup(boxTakhal , options=["تخلخل", "حجم فضای خالی سنگ یا رسوب" , "حجم کل فضای کل سنگ یا رسوب"] ,selected="تخلخل",command=updateBtnTakhal)
+boxTakhal = Box(app, visible=False, width='fill')
+btnTakhal = ButtonGroup(boxTakhal, options=[
+                        "تخلخل", "حجم فضای خالی سنگ یا رسوب", "حجم کل فضای کل سنگ یا رسوب"], selected="تخلخل", command=updateBtnTakhal)
 Text(boxTakhal, "لطفا دو مقدار را وارد کنید")
-Text(boxTakhal,'تخلخل')
-txtTakhal = TextBox(boxTakhal, width='fill' , command=takhalParametersChange,enabled=False)
-Text(boxTakhal,'حجم فضای خلی سنگ یا رسوب ')
-txtVsang = TextBox(boxTakhal , width='fill' , command=takhalParametersChange)
-Text(boxTakhal , 'حجم کل فضای سنگ یا رسوب')
-txtVkol = TextBox(boxTakhal , width='fill' , command=takhalParametersChange)
-btnExiTakhal=PushButton(boxTakhal,command=hideBox,text="صفحه ی اصلی")
+Text(boxTakhal, 'تخلخل')
+txtTakhal = TextBox(boxTakhal, width='fill',
+                    command=takhalParametersChange, enabled=False)
+Text(boxTakhal, 'حجم فضای خلی سنگ یا رسوب ')
+txtVsang = TextBox(boxTakhal, width='fill', command=takhalParametersChange)
+Text(boxTakhal, 'حجم کل فضای سنگ یا رسوب')
+txtVkol = TextBox(boxTakhal, width='fill', command=takhalParametersChange)
+btnExiTakhal = PushButton(boxTakhal, command=hideBox, text="صفحه ی اصلی")
 
 # Menu bar
 about_menubar = MenuBar(app, toplevel=["منو"], options=[[["درباره", about]]])
 
-# menu_title()
 app.display()
